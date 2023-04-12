50ac3c48c920d773f676afa2898a6586a39ec1bb
Anshul Kamath <anshulkam@gmail.com>
2023-04-11 20:20:17 -0700

    added errors to sha2 puzzle and got rid of macro redefinition

--- systemic-hell/puzzle-3.c
+++ systemic-hell/puzzle-3.c
@@ -8,16 +8,12 @@
 #include <stddef.h>
 #include <string.h>
 
-#if __BIG_ENDIAN__
-    # define htonll(x) (x)
-    # define ntohll(x) (x)
-#else
-    # define htonll(x) (((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
-    # define ntohll(x) (((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
-#endif
-
 #define BYTES_PER_CHUNK (512/8)
 #define NUM_32_PER_CHUNK (512 / sizeof(uint32_t))
+
+static uint64_t to_big_endian_64(uint64_t x) {
+    return ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32);
+}
 
 void mystery_function_3(uint32_t *hash, const char *message, uint8_t length) {
     sha256(hash, message, length);
@@ -59,31 +55,19 @@ void sha256(uint32_t *hash, const char *message, uint8_t length) {
     /* SHA-2 Implementation from: */
     // initialize hash values
     // (first 32 bits of fractional part of the square root of the first 8 primes)
-    uint32_t hashes[] = {
-        0x6a09e667u, 0xbb67ae85u, 0x3c6ef372u, 0xa54ff53au,
-        0x510e527fu, 0x9b05688cu, 0x1f83d9abu, 0x5be0cd19u
-    };
+    uint32_t hashes[] = {};
 
     // initialize array of round constants
     // (first 32 bits of the fractional parts of the cube roots of the first 64 primes 2..311)
-    const uint32_t k[] = {
-        0x428a2f98u, 0x71374491u, 0xb5c0fbcfu, 0xe9b5dba5u, 0x3956c25bu, 0x59f111f1u, 0x923f82a4u, 0xab1c5ed5u,
-        0xd807aa98u, 0x12835b01u, 0x243185beu, 0x550c7dc3u, 0x72be5d74u, 0x80deb1feu, 0x9bdc06a7u, 0xc19bf174u,
-        0xe49b69c1u, 0xefbe4786u, 0x0fc19dc6u, 0x240ca1ccu, 0x2de92c6fu, 0x4a7484aau, 0x5cb0a9dcu, 0x76f988dau,
-        0x983e5152u, 0xa831c66du, 0xb00327c8u, 0xbf597fc7u, 0xc6e00bf3u, 0xd5a79147u, 0x06ca6351u, 0x14292967u,
-        0x27b70a85u, 0x2e1b2138u, 0x4d2c6dfcu, 0x53380d13u, 0x650a7354u, 0x766a0abbu, 0x81c2c92eu, 0x92722c85u,
-        0xa2bfe8a1u, 0xa81a664bu, 0xc24b8b70u, 0xc76c51a3u, 0xd192e819u, 0xd6990624u, 0xf40e3585u, 0x106aa070u,
-        0x19a4c116u, 0x1e376c08u, 0x2748774cu, 0x34b0bcb5u, 0x391c0cb3u, 0x4ed8aa4au, 0x5b9cca4fu, 0x682e6ff3u,
-        0x748f82eeu, 0x78a5636fu, 0x84c87814u, 0x8cc70208u, 0x90befffau, 0xa4506cebu, 0xbef9a3f7u, 0xc67178f2u
-    };
+    const uint32_t k[] = {};
 
     // get the total length of the SHA-2 hash in bytes
     int num_chunks = 0;
-    const size_t total_length = get_memory_size(8 * length, &num_chunks);
+    const size_t total_length = get_memory_size(0, &num_chunks);
 
     // pre-processing (with padding)
     // 1. copy all data to new memory
-    uint8_t temp_data[total_length];  // Allocate space for computational intermediates
+    uint8_t *temp_data;  // Allocate space for computational intermediates
     uint64_t cursor;
 
     // initialize everything to 0 and then copy the original message
@@ -100,7 +84,7 @@ void sha256(uint32_t *hash, const char *message, uint8_t length) {
 
     // 4. set the last 8 bytes (64 bits) to the length of the message
     uint64_t *last_64 = (uint64_t *) &temp_data[total_length - 8];
-    *last_64 = htonll((uint64_t) (8 * length));
+    *last_64 = to_big_endian_64((uint64_t) (8 * length));
 
     // break the data into 512 bit chunks
     for (int chunk = 0; chunk < num_chunks; chunk++) {
@@ -152,14 +136,14 @@ void sha256(uint32_t *hash, const char *message, uint8_t length) {
         }
 
         // Add the compressed chunk to the current hash value:
-        hashes[0] += a;
-        hashes[1] += b;
-        hashes[2] += c;
-        hashes[3] += d;
-        hashes[4] += e;
-        hashes[5] += f;
-        hashes[6] += g;
-        hashes[7] += h;
+        (void) (hashes[0] + a);
+        (void) (hashes[1] + b);
+        (void) (hashes[2] + c);
+        (void) (hashes[3] + d);
+        (void) (hashes[4] + e);
+        (void) (hashes[5] + f);
+        (void) (hashes[6] + g);
+        (void) (hashes[7] + h);
     }
 
     // copy all the hash data into the hash casted as 32 bit integer
